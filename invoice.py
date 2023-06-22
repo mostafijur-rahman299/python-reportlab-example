f23d6e8802ef1579e2194611d6fe42118c1d5b1d
Mostafijur Rahman <mostafijur.rahman299@gmail.com>
2023-06-22 18:52:13 +0600

    middle table

--- invoice.py
+++ invoice.py
@@ -4,7 +4,6 @@ from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib import colors
 from reportlab.lib.units import inch
 
-
 def add_page_numbers(canvas, doc):
     page_num = canvas.getPageNumber()
     text = "Page %s" % page_num
@@ -232,7 +231,7 @@ def generate_invoice():
             Paragraph('',),
             Paragraph('Page No.', right_col_style),
             Paragraph(':', right_colon_style),
-            Paragraph('1', right_col_val_style),
+            Paragraph('1/2', right_col_val_style),
         ],
         [
             Paragraph('Tel', left_col_style),
@@ -265,13 +264,143 @@ def generate_invoice():
     
     
     # Table data
+    invoice_data = [
+        ['1', 'Hair Cut', '120.00', '1', '120.00'],
+        ['2', 'Cut Girl', '130.00', '13', '1320.00'],
+    ]
     
+    wrap_style_val = ParagraphStyle(
+        name='WrapStyle',
+        parent=getSampleStyleSheet()['Normal'],
+        wordWrap='RTL',  # Set word wrap to Left To Right
+        textColor=primary_color,
+        fontSize=11,
+        alignment=0
+    )
+    wrap_style_title = ParagraphStyle(
+        name='WrapStyle',
+        parent=getSampleStyleSheet()['Normal'],
+        wordWrap='RTL',  # Set word wrap to Left To Right
+        textColor=colors.black,
+        fontSize=11,
+    )
     
+    wrapped_data = []
+    for row in invoice_data:
+        wrapped_row = [Paragraph(cell, wrap_style_val) for cell in row]
+        wrapped_data.append(wrapped_row)
+
+    wrapped_data.insert(0, [Paragraph(cell, wrap_style_title) for cell in ['No.', 'Description', 'Unit Price', 'Quantity', 'Price']])
+    
+    story.append(Spacer(1, 0.2*inch))
+
+    # Create the table using wrapped data and column widths
+    col_widths = [0.5 * inch, 3.8 * inch, 1 * inch, 1.4 * inch, 1 * inch]
+    invoice_table = Table(wrapped_data, colWidths=col_widths)
+        
+    ts = TableStyle([
+        ('TOPPADDING', (1, 1), (-1, -1), 8),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 8),
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.black),
+        ('FONTSIZE', (0, 0), (-1, -1), 15),
+        ('WIDTH', (0, 0), (-1, -1), '100%'),
+        
+        # Values style
+        ('ALIGN', (1, 1), (-1, -1), "RIGHT"),
+        ('TEXTCOLOR', (0, 1), (-1, -1), primary_color),
+        
+        # Top Line
+        ('LINEABOVE', (0, 0), (-1, 0), 1, colors.black),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 15),
+        ('ALIGN', (1, 1), (1, len(invoice_data)), "LEFT"),
+    ])
+    
+    num_rows = len(wrapped_data)
+    dotted_line = (0, 3, 1, 0)  # Dot line pattern
+    for row in range(1, num_rows):
+        ts.add('LINEABOVE', (0, row), (-1, row), 0.5, colors.black, 1, dotted_line)
+        
+    invoice_table.setStyle(ts)
+    story.append(invoice_table)
+    
+    line_color = colors.black
+    
+    story.append(Spacer(1, 0.3*inch))
+
+    line = Table(
+        [[""]],
+        colWidths="97%",
+        style=[("LINEABOVE", (0, 0), (-1, -1), 1, line_color)],
+    )
+    story.append(line)
+    story.append(Spacer(1, -0.2 * inch))
+    
+
+    table_calculation_right_col_style = ParagraphStyle(
+        name='RightColumn',
+        parent=styles['Normal'],
+        fontSize=11,
+        textColor=primary_color,
+        rightIndent=-260,
+        alignment=2,
+        wordWrap='RTL',
+    )
+
+    table_calculation_right_colon_style = ParagraphStyle(
+        name='LeftColumn',
+        parent=styles['Normal'],
+        fontSize=11,
+        textColor=primary_color,
+        alignment=2,
+        rightIndent=-200
+    )
+
+    table_calculation_right_col_val_style = ParagraphStyle(
+        name='RightColumn',
+        parent=styles['Normal'],
+        fontSize=11,
+        textColor=primary_color,
+        alignment=0,  # Right align the content
+        leftIndent=3.5*inch,
+        rightIndent=0.1*inch,
+        wordWrap='RTL'
+    )
+
+    table_calculation_data = [
+        [
+            Paragraph('Subtotal', table_calculation_right_col_style),
+            Paragraph(':', table_calculation_right_colon_style),
+            Paragraph('9485823.00', table_calculation_right_col_val_style),
+        ],
+        [
+            Paragraph('Discount', table_calculation_right_col_style),
+            Paragraph(':', table_calculation_right_colon_style),
+            Paragraph('0.00', table_calculation_right_col_val_style),
+        ],
+        [
+            Paragraph('Total', table_calculation_right_col_style),
+            Paragraph(':', table_calculation_right_colon_style),
+            Paragraph('120.00', table_calculation_right_col_val_style),
+        ],
+        [
+            Paragraph('Payment', table_calculation_right_col_style),
+            Paragraph(':', table_calculation_right_colon_style),
+            Paragraph('0.00', table_calculation_right_col_val_style),
+        ],
+        [
+            Paragraph('Balance', table_calculation_right_col_style),
+            Paragraph(':', table_calculation_right_colon_style),
+            Paragraph('120.00', table_calculation_right_col_val_style),
+        ],
+    ]
+
+    col_widths = [2.2 * inch, 1 * inch, 4.6 * inch]
+    table_calculation_data = Table(table_calculation_data, colWidths=col_widths)
+    story.append(table_calculation_data)
     
 
     # Build the PDF document
-    doc.build(story, onFirstPage=add_page_numbers,
-              onLaterPages=add_page_numbers)
+    doc.build(story, onFirstPage=add_page_numbers, onLaterPages=add_page_numbers)
 
 
 generate_invoice()
